18e9acd4c04663a3dec7bf91a8590f95fa7c48f0
kartik <kartik.ramesh@icloud.com>
2024-01-03 10:02:33 -0500

    connected flask app to react

--- kunai/src/App.js
+++ kunai/src/App.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
-
 function App() {
   const [username, setUsername] = useState('');
   const [token, setToken] = useState('');
   const [repositories, setRepositories] = useState([]);
   const [loading, setLoading] = useState(false);
-
   const fetchData = async () => {
     try {
       setLoading(true); // Set loading to true when starting the fetch
       const response = await fetch(`http://127.0.0.1:5000/?username=${username}&token=${token}`);
       const data = await response.json();
+      console.log('Data:',data)
       setRepositories(data.repositories || []);
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -42,30 +41,33 @@ function App() {
           <h2>Repositories:</h2>
           {loading && <div className="loader">Loading...</div>}
           <ul>
-            {repositories.map((repo) => (
-              <li key={repo.Name} className="repo-item">
-                <strong>{repo.Name}</strong>
-                <p className="repo-description">{repo.Description}</p>
-                <a href={repo.URL} target="_blank" rel="noopener noreferrer">
-                  {repo.URL}
-                </a>
-                {repo.Branches && (
-                  <div className="repo-branches">
-                    <strong>Branches:</strong>
-                    <ul>
-                      {repo.Branches.map((branch) => (
-                        <li key={branch.name}>{branch.name}</li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </li>
-            ))}
+            {repositories.length === 0 ? (
+              <p>No repositories found.</p>
+            ) : (
+              repositories.map((repo) => (
+                <li key={repo.Name} className="repo-item">
+                  <strong>{repo.Name}</strong>
+                  <p className="repo-description">{repo.Description}</p>
+                  <a href={repo.URL} target="_blank" rel="noopener noreferrer">
+                    {repo.URL}
+                  </a>
+                  {repo.Branches && (
+                    <div className="repo-branches">
+                      <strong>Branches:</strong>
+                      <ul>
+                        {repo.Branches.map((branch) => (
+                          <li key={branch.name}>{branch.name}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </li>
+              ))
+            )}
           </ul>
         </main>
       </div>
     </div>
   );
 }
-
 export default App;
